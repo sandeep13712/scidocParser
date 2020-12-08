dd9aa9a6c6ef440bc91c4f331473a640f7b2b3a7
Sandeep Kumar <sandeep25789@gmail.com>
2020-12-08 10:26:42 -0500

    modelgenerated

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # scidocParser
+This script uses tensorflow NLP toolkit to classify type of sentences in a research paper. Trained model is then exported to json and exported model is used to power our online paper parser available at https://www.sanmed.ca/pages/paperparser
--- sciDocParser.py
+++ sciDocParser.py
@@ -4,6 +4,32 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 import pandas as pd
+import numpy as np
+
+import tensorflowjs as tfjs
+
+# import matplotlib.pyplot as plt
+
+
+def plot_graphs(history, string):
+  plt.plot(history.history[string])
+  plt.plot(history.history['val_'+string])
+  plt.xlabel("Epochs")
+  plt.ylabel(string)
+  plt.legend([string, 'val_'+string])
+  plt.show()
+
+
+def encodeLabel(labels):
+    sentenceTypes = ['irrelevant','introduction','observation','perturbation']
+    numLabels = []
+    for l in labels:
+        print(l)
+        try:
+            numLabels.append(sentenceTypes.index(l))
+        except:
+            numLabels.append(0)
+    return numLabels
 
 
 # tokenizer = Tokenizer(num_words = 100, oov_token='<OOV>')
@@ -54,13 +80,12 @@ maxLength = 20
 tokenizer = Tokenizer(num_words=volcabularySize,oov_token=OOV)
 tokenizer.fit_on_texts(trainInputs)
 
+wordList = tokenizer.word_index
+
+print(wordList)
 
 trainSequences = tokenizer.texts_to_sequences(trainInputs)
 trainSequences_padded = pad_sequences(trainSequences, maxlen=maxLength, truncating='pre', padding='pre')
-
-
-testSequences = tokenizer.texts_to_sequences(testInputs)
-testSequences_padded = pad_sequences(testSequences, maxlen=maxLength, truncating='pre', padding='pre')
 
 validateSequences = tokenizer.texts_to_sequences(validateInputs)
 validateSequences_padded = pad_sequences(validateSequences, maxlen=maxLength, truncating='pre', padding='pre')
@@ -68,7 +93,6 @@ validateSequences_padded = pad_sequences(validateSequences, maxlen=maxLength, tr
 # validateSequences = tokenizer.texts_to_sequences(validateInputs)
 
 print(trainSequences_padded)
-print(testSequences_padded)
 print(validateSequences_padded)
 
 #mdel
@@ -76,13 +100,39 @@ model = tf.keras.Sequential([
     tf.keras.layers.Embedding(input_dim=volcabularySize,output_dim= 16,input_length=maxLength),
     tf.keras.layers.GlobalAveragePooling1D(),
     tf.keras.layers.Dense(units=24,activation='relu'),
-    tf.keras.layers.Dense(units=2,activation='sigmoid'),
+    tf.keras.layers.Dense(units=1,activation='sigmoid'),
 ])
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 model.summary()
 
-num_epochs = 2000
+
+import numpy as np
+trainSequences_padded = np.array(trainSequences_padded)
+trainLabels = np.array(encodeLabel(trainLabels))
+validateSequences_padded = np.array(validateSequences_padded)
+validateLabels = np.array(encodeLabel(validateLabels))
+
+print(trainLabels)
+print(validateLabels)
+
+num_epochs = 200
 history = model.fit(trainSequences_padded, trainLabels, epochs=num_epochs, validation_data=(validateSequences_padded, validateLabels), verbose=2)
+  
+# plot_graphs(history, "accuracy")
+# plot_graphs(history, "loss")
+
+
+testSequences = tokenizer.texts_to_sequences(testInputs)
+testSequences_padded = pad_sequences(testSequences, maxlen=maxLength, truncating='pre', padding='pre')
+print(testSequences_padded)
+
+testSequences_padded = np.array(testSequences_padded)
+testLabels = np.array(encodeLabel(testLabels))
+print(model.predict(testSequences_padded))
+
+
+#if model provide reasonable prediction
+tfjs.converters.save_keras_model(model, '/tmp/sciDocParser/')
 
